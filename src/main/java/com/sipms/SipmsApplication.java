3c6b36ef4531241f1a3e7332a47289245347b034
James Okutoyi <okutoyijemo@gmail.com>
2025-12-02 21:54:23 +0300

    initialize project dependencies

---
+++ src/main/java/com/sipms/SipmsApplication.java
@@ -0,0 +1,13 @@
+package com.sipms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipmsApplication.class, args);
+	}
+
+}
